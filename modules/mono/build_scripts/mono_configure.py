7e0a55f4a79e7b72ecb284e08e9fa04f83d12fde
milky67 <milkymilkydevs@gmail.com>
2026-08-23 12:41:57 +0800

    Enhance mono_configure.py for Android and iOS support
    Added support for Android and iOS platforms in the configuration script.

--- modules/mono/build_scripts/mono_configure.py
+++ modules/mono/build_scripts/mono_configure.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+
 def is_desktop(platform):
     return platform in ["windows", "macos", "linuxbsd"]
 
@@ -6,20 +10,34 @@ def is_unix_like(platform):
     return platform in ["macos", "linuxbsd", "android", "ios"]
 
 
-# BINAGO NATIN ITO: Idinagdag natin ang "android" para payagan ang editor build
 def module_supports_tools_on(platform):
+    # PINAYAGAN ANG ANDROID DITO
     return is_desktop(platform) or platform == "android"
 
 
 def configure(env, env_mono):
-    # is_android = env["platform"] == "android"
-    # is_web = env["platform"] == "web"
-    # is_ios = env["platform"] == "ios"
-    # is_ios_sim = is_ios and env["arch"] in ["x86_32", "x86_64"]
+    is_android = env["platform"] == "android"
+    is_ios = env["platform"] == "ios"
+    is_web = env["platform"] == "web"
 
     if env.editor_build:
         if not module_supports_tools_on(env["platform"]):
             raise RuntimeError("This module does not currently support building for this platform for editor builds.")
-        
-        # Pwede mong i-keep ito o i-disable muna ang hot reload kung magkaproblema sa Android
         env_mono.Append(CPPDEFINES=["GD_MONO_HOT_RELOAD"])
+
+    if is_android:
+        env_mono.Append(CPPDEFINES=["ANDROID_ENABLED"])
+    elif is_ios:
+        env_mono.Append(CPPDEFINES=["IOS_ENABLED"])
+    elif is_web:
+        env_mono.Append(CPPDEFINES=["JAVASCRIPT_ENABLED"])
+
+    if is_unix_like(env["platform"]):
+        env_mono.Append(CPPDEFINES=["UNIX_ENABLED"])
+
+    if env["platform"] == "windows":
+        env_mono.Append(CPPDEFINES=["WINDOWS_ENABLED"])
+    elif env["platform"] == "macos":
+        env_mono.Append(CPPDEFINES=["MACOS_ENABLED"])
+    elif env["platform"] == "linuxbsd":
+        env_mono.Append(CPPDEFINES=["LINUXBSD_ENABLED"])
