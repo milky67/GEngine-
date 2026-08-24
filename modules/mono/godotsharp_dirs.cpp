e431b1c65a3ff7803337cae5aa01965ed942420a
milky67 <milkymilkydevs@gmail.com>
2026-08-24 11:05:31 +0800

    Refactor directory checks and path handling

--- modules/mono/godotsharp_dirs.cpp
+++ modules/mono/godotsharp_dirs.cpp
@@ -74,10 +74,9 @@ String _get_mono_user_dir() {
 	} else {
 		String settings_path = OS::get_singleton()->get_data_path().path_join(OS::get_singleton()->get_godot_dir_name());
 
-		// Self-contained mode if a `._sc_` or `_sc_` file is present in executable dir.
 		String exe_dir = OS::get_singleton()->get_executable_path().get_base_dir();
 		Ref<DirAccess> d = DirAccess::create_for_path(exe_dir);
-		if (d->file_exists("._sc_") || d->file_exists("_sc_")) {
+		if (d.is_valid() && (d->file_exists("._sc_") || d->file_exists("_sc_"))) {
 			settings_path = exe_dir.path_join("editor_data");
 		}
 
@@ -85,7 +84,7 @@ String _get_mono_user_dir() {
 		if (OS::get_singleton()->has_feature("macos") && exe_dir.ends_with("MacOS") && exe_dir.path_join("..").simplify_path().ends_with("Contents")) {
 			exe_dir = exe_dir.path_join("../../..").simplify_path();
 			d = DirAccess::create_for_path(exe_dir);
-			if (d->file_exists("._sc_") || d->file_exists("_sc_")) {
+			if (d.is_valid() && (d->file_exists("._sc_") || d->file_exists("_sc_"))) {
 				settings_path = exe_dir.path_join("editor_data");
 			}
 		}
@@ -145,18 +144,15 @@ private:
 			String global_dir = ProjectSettings::get_singleton()->globalize_path(p_dir);
 			if (!DirAccess::exists(global_dir)) {
 				DirAccess::make_dir_recursive_absolute(global_dir);
-				print_verbose(".NET/Android: Created project directory: " + global_dir);
 			}
 		}
 	}
 
 	_GodotSharpDirs() {
-		String res_data_dir = ProjectSettings::get_singleton()->get_project_data_path().path_join("mono");
+		String res_data_dir = ProjectSettings::get_singleton() ? ProjectSettings::get_singleton()->get_project_data_path().path_join("mono") : "res://.godot/mono";
 		res_metadata_dir = res_data_dir.path_join("metadata");
-
 		res_temp_assemblies_dir = res_data_dir.path_join("temp").path_join("bin").path_join(_get_expected_build_config());
 
-		// Awtomatikong likhain ang kailangang project mono folders
 		_ensure_directory_exists(res_metadata_dir);
 		_ensure_directory_exists(res_temp_assemblies_dir);
 
@@ -181,7 +177,6 @@ private:
 		build_logs_dir = mono_user_dir.path_join("build_logs");
 
 #ifdef ANDROID_ENABLED
-		// Sa Android Editor, hanapin ang bundled GodotSharp sa storage o internal assets
 		Vector<String> probe_roots;
 		probe_roots.push_back("res://GodotSharp");
 		probe_roots.push_back("/storage/emulated/0/GEngine/GodotSharp");
@@ -193,7 +188,6 @@ private:
 				data_dir_root = candidate;
 				data_editor_tools_dir = data_dir_root.path_join("Tools");
 				api_assemblies_base_dir = data_dir_root.path_join("Api");
-				print_verbose(".NET/Android: Located GodotSharp directory at: " + data_dir_root);
 				break;
 			}
 		}
@@ -205,7 +199,13 @@ private:
 			api_assemblies_base_dir = res_dir.path_join("GodotSharp").path_join("Api");
 		}
 #endif
-		api_assemblies_dir = api_assemblies_base_dir.path_join(GDMono::get_expected_api_build_config());
+		// Subfolder check: tingnan kung nasa Api/Debug o direkta sa Api/
+		String candidate_config_dir = api_assemblies_base_dir.path_join(GDMono::get_expected_api_build_config());
+		if (DirAccess::exists(candidate_config_dir)) {
+			api_assemblies_dir = candidate_config_dir;
+		} else {
+			api_assemblies_dir = api_assemblies_base_dir;
+		}
 #else // TOOLS_ENABLED
 		String platform = _get_platform_name();
 		String arch = Engine::get_singleton()->get_architecture_name();
@@ -213,13 +213,12 @@ private:
 		String packed_path = "res://.godot/mono/publish/" + arch;
 #ifdef ANDROID_ENABLED
 		api_assemblies_dir = packed_path;
-		print_verbose(".NET: Android platform detected. Setting api_assemblies_dir directly to pck path: " + api_assemblies_dir);
 #else
 		if (DirAccess::exists(packed_path)) {
 			String data_dir_root = OS::get_singleton()->get_cache_path().path_join("data_" + appname_safe + "_" + platform + "_" + arch);
 			bool has_data = false;
 			if (!has_data) {
-				String global_packed = ProjectSettings::get_singleton()->globalize_path(packed_path);
+				String global_packed = ProjectSettings::get_singleton() ? ProjectSettings::get_singleton()->globalize_path(packed_path) : packed_path;
 				if (global_packed.is_absolute_path() && FileAccess::exists(global_packed.path_join(".dotnet-publish-manifest"))) {
 					data_dir_root = global_packed;
 					has_data = true;
@@ -238,12 +237,14 @@ private:
 				Ref<DirAccess> da;
 				if (DirAccess::exists(data_dir_root)) {
 					da = DirAccess::open(data_dir_root);
-					ERR_FAIL_COND(da.is_null());
-					ERR_FAIL_COND(da->erase_contents_recursive() != OK);
+					if (da.is_valid()) {
+						da->erase_contents_recursive();
+					}
 				}
 				da = DirAccess::create_for_path(packed_path);
-				ERR_FAIL_COND(da.is_null());
-				ERR_FAIL_COND(da->copy_dir(packed_path, data_dir_root) != OK);
+				if (da.is_valid()) {
+					da->copy_dir(packed_path, data_dir_root);
+				}
 			}
 			api_assemblies_dir = data_dir_root;
 		} else {
