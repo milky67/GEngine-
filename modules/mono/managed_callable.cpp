ab0c4f97fe7d0862cc20dc876b2739b555484b63
milky67 <milkymilkydevs@gmail.com>
2026-08-24 12:43:30 +0800

    Update managed_callable.cpp

--- modules/mono/managed_callable.cpp
+++ modules/mono/managed_callable.cpp
@@ -1,32 +1,6 @@
 /**************************************************************************/
 /*  managed_callable.cpp                                                  */
 /**************************************************************************/
-/*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
-/**************************************************************************/
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
-/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
-/*                                                                        */
-/* Permission is hereby granted, free of charge, to any person obtaining  */
-/* a copy of this software and associated documentation files (the        */
-/* "Software"), to deal in the Software without restriction, including    */
-/* without limitation the rights to use, copy, modify, merge, publish,    */
-/* distribute, sublicense, and/or sell copies of the Software, and to     */
-/* permit persons to whom the Software is furnished to do so, subject to  */
-/* the following conditions:                                              */
-/*                                                                        */
-/* The above copyright notice and this permission notice shall be         */
-/* included in all copies or substantial portions of the Software.        */
-/*                                                                        */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
-/**************************************************************************/
 
 #include "managed_callable.h"
 
@@ -54,7 +28,6 @@ bool ManagedCallable::compare_equal(const CallableCustom *p_a, const CallableCus
 		return a->delegate_handle.value == b->delegate_handle.value;
 	}
 
-	// Call Delegate's 'Equals' safely
 	return GDMonoCache::managed_callbacks.DelegateUtils_DelegateEquals(
 			a->delegate_handle, b->delegate_handle);
 }
@@ -132,7 +105,6 @@ void ManagedCallable::release_delegate_handle() {
 	}
 }
 
-// Why you do this clang-format...
 /* clang-format off */
 ManagedCallable::ManagedCallable(GCHandleIntPtr p_delegate_handle, void *p_trampoline, ObjectID p_object_id) :
 		delegate_handle(p_delegate_handle), trampoline(p_trampoline), object_id(p_object_id) {
