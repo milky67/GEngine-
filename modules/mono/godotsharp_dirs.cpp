3eefc09fb64f24b4068f1905f722065b97b52120
milky67 <milkymilkydevs@gmail.com>
2026-08-24 12:44:34 +0800

    Remove copyright notice from godotsharp_dirs.cpp

--- modules/mono/godotsharp_dirs.cpp
+++ modules/mono/godotsharp_dirs.cpp
@@ -1,32 +1,6 @@
 /**************************************************************************/
 /*  godotsharp_dirs.cpp                                                   */
 /**************************************************************************/
-/*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
-/**************************************************************************/
-/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
-/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
-/*                                                                        */
-/* Permission is hereby granted, free of charge, to any person obtaining  */
-/* a copy of this software and associated documentation files (the        */
-/* "Software"), to deal in the Software without restriction, including    */
-/* without limitation the rights to use, copy, modify, merge, publish,    */
-/* distribute, sublicense, and/or sell copies of the Software, and to     */
-/* permit persons to whom the Software is furnished to do so, subject to  */
-/* the following conditions:                                              */
-/*                                                                        */
-/* The above copyright notice and this permission notice shall be         */
-/* included in all copies or substantial portions of the Software.        */
-/*                                                                        */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
-/**************************************************************************/
 
 #include "godotsharp_dirs.h"
 
@@ -199,7 +173,6 @@ private:
 			api_assemblies_base_dir = res_dir.path_join("GodotSharp").path_join("Api");
 		}
 #endif
-		// Subfolder check: tingnan kung nasa Api/Debug o direkta sa Api/
 		String candidate_config_dir = api_assemblies_base_dir.path_join(GDMono::get_expected_api_build_config());
 		if (DirAccess::exists(candidate_config_dir)) {
 			api_assemblies_dir = candidate_config_dir;
@@ -308,3 +281,4 @@ String get_data_editor_tools_dir() {
 #endif
 
 } // namespace GodotSharpDirs
+
